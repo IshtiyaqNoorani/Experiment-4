9d2a0ec59b40daa8d18235755fbbab1fc5c76f8d
ishtiyaq noorani <ishtiyaqnoorani@mac.local>
2026-02-09 11:55:43 +0530

    Integrated add, sub, and mul modules into main

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#Calculator
+from add import add
+from sub import sub
+from mul import mul
+
+a = 10
+b = 5
+
+print("Addition:", add(a, b))
+print("Subtraction:", sub(a, b))
+print("Multiplication:", mul(a, b))
+
